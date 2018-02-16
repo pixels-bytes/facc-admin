888ee6e22c5e2c6ca5ac551b07973272fa5dff9b
pixels-bytes <adam@pixelsandbytes.co>
2018-02-16 15:25:20 +0100

    Nav drawer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-toolbar>
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+        class="hidden-sm-and-up"
+      ></v-toolbar-side-icon>
       <v-toolbar-title>FACC Admin</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn flat>
           <v-icon left>restaurant</v-icon>
           View Courses
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>restaurant</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Courses</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <main>
 
     </main>
@@ -20,8 +34,8 @@
 export default {
   data() {
     return {
+      drawer: false,
       clipped: false,
-      drawer: true,
       fixed: false,
       items: [{
         icon: 'bubble_chart',
