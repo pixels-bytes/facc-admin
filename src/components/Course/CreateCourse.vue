b35ea7623acfb5755b44b208bdbdfa3810738a0b
pixels-bytes <adam@pixelsandbytes.co>
2018-02-16 22:56:48 +0100

    Create Course form added with validation but no effect

--- src/components/Course/CreateCourse.vue
+++ src/components/Course/CreateCourse.vue
@@ -1,5 +1,104 @@
 <template>
-    <div>
-        <p>Create Course page</p>
-    </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h2>Create a New Course</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="title"
+                label="Title"
+                id="title"
+                v-model="title"
+                required
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="category"
+                label="Category"
+                id="category"
+                v-model="category"
+                required
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                id="location"
+                v-model="location"
+                required
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="start-date"
+                label="Start Date"
+                id="start-date"
+                v-model="startDate"
+                required
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="end-date"
+                label="End Date"
+                id="end-date"
+                v-model="endDate"
+                required
+              >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary" :disabled="!formIsValid">Create Course</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      category: '',
+      location: '',
+      startDate: '',
+      endDate: '',
+    };
+  },
+  computed: {
+    formIsValid() {
+      return this.title !== '' &&
+        this.category !== '' &&
+        this.location !== '' &&
+        this.startDate !== '' &&
+        this.endDate !== ''; 
+    }
+  },
+}
+</script>
