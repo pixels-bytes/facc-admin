e87d65403ea89443a3e3523dcf9c87505adb2af8
pixels-bytes <adam@pixelsandbytes.co>
2018-02-16 15:40:45 +0100

    Populate menus from array

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <v-app dark>
+
     <v-toolbar>
-      <v-toolbar-side-icon
-        @click="drawer = !drawer"
-        class="hidden-sm-and-up"
-      ></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>FACC Admin</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
+      <v-toolbar-items class="hidden-xs-only" v-for="item in menuItems" :key="item.title">
         <v-btn flat>
-          <v-icon left>restaurant</v-icon>
-          View Courses
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+
     <v-navigation-drawer v-model="drawer">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title ">
           <v-list-tile-action>
-            <v-icon>restaurant</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Courses</v-list-tile-content>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
     <main>
 
     </main>
@@ -35,16 +35,12 @@ export default {
   data() {
     return {
       drawer: false,
-      clipped: false,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire',
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      menuItems: [
+        { icon: 'restaurant', title: 'View Courses' },
+        { icon: 'person', title: 'Profile' },
+        { icon: 'face', title: 'Sign Up' },
+        { icon: 'lock_open', title: 'Login' }
+]
     };
   },
   name: 'App',
--- src/main.js
+++ src/main.js
@@ -3,16 +3,9 @@ import App from './App';
 import router from './router';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import colors from 'vuetify/es5/util/colors';
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify, {
-  theme: {
-    primary: colors.purple.base,
-    secondary: colors.grey.darken1,
-    accent: colors.shades.black,
-    error: colors.red.accent3
-  }
-})
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
