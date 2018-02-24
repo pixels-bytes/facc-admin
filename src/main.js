9faca5ceee06f2b714dca4c678202110dc09b539
pixels-bytes <adam@pixelsandbytes.co>
2018-02-24 18:32:12 +0100

    Reorganise mainjs

--- src/main.js
+++ src/main.js
@@ -1,26 +1,37 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
-import VueFirestore from 'vue-firestore';
+
+
+/* Firebase */
 import Firebase from '@firebase/app';
+import VueFirestore from 'vue-firestore';
 import '@firebase/auth';
+
+/* Vuetify */
+import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
+/* App */
 import App from './App';
 import router from './router';
 import store from './store';
+
+/* Shared */
 import DateFilter from './filters/date';
 import AlertCmp from './components/Shared/Alert';
-import theme from './config/theme';
-import './firebase/firebaseInit';
 
+/* Config */
+import './firebase/firebaseInit';
+import theme from './config/theme';
+
+/* Wireup Middleware */
 Vue.use(VueFirestore);
 Vue.use(Vuetify, theme);
+Vue.filter('date', DateFilter);
+Vue.component('app-alert', AlertCmp);
 
 // turns off the 'You are running Vue in development mode.' msg
 Vue.config.productionTip = false;
 
-Vue.filter('date', DateFilter);
-Vue.component('app-alert', AlertCmp);
 
 /* eslint-disable no-new */
 new Vue({
