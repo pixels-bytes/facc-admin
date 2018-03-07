99e8f3d880e6586108ae8f3d0238a6ecc128b8e1
pixels-bytes <adam@pixelsandbytes.co>
2018-03-07 12:49:14 +0100

    Set custom claim of admin: false on new user

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.setAdminClaimOnNewUser = functions.auth.user().onCreate(
+  (event) => {
+    const user = event.data;
+    admin.auth().setCustomUserClaims(user.uid, { admin: false });
+  });
